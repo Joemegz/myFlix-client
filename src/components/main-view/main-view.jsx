3e86b61f19ab5c86ec0f4bdad9d82f31b020b921
Joe Megyery <104165842+Joemegz@users.noreply.github.com>
2023-03-26 15:20:18 -0500

    exercise 3.6

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,27 +3,24 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export const MainView = () => {
-  const storedUser = JSON.parse(localStorage.getItem("user"));
-  const storedToken = localStorage.getItem("token");
+  // const storedUser = JSON.parse(localStorage.getItem("user"));
+  // const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
 
-  
-
-
-
-  useEffect(() => { 
-
+  useEffect(() => {
     if (!token) {
       return;
     }
 
     fetch("https://myflix2023.herokuapp.com/movies", {
-      headers: { Authorization: `Bearer ${token}` }
+      headers: { Authorization: `Bearer ${token}` },
     }) //fetching endpoint
       .then((response) => response.json()) //response parsed in json
       .then((data) => {
@@ -37,91 +34,52 @@ export const MainView = () => {
             image: movie.ImagePath,
             description: movie.Description,
             genre: movie.Genre.Name,
-            director: movie.Director.Name
+            director: movie.Director.Name,
           };
         });
         setMovies(moviesFromApi);
       });
   }, [token]);
- 
-  if (!user) {
-    console.log("testing")
-    return (
-      <>
-      <LoginView onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }} />
-      or
-      <SignupView/>
-      </>
-    );
-  }
-
-  // Passing an empty dependency array  as a second argument to useEffect tells React that your callback doesn’t depend on any value changes in props or state, so it never needs to rerun.
-<button onClick={() => { setUser(null); setToken(null); }}>Logout</button>
-  if (!user) {
-    return (
-      <LoginView
-        onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }}
-      />
-    );
-  }
-
-  if (selectedMovie) {
-    return (
-      <>
-      <button
-      onClick={() => {
-        setUser(null);
-      }}
-      > 
-      Logout
-      </button>
-      <MovieView
-        movie={selectedMovie}
-        onBackClick={() => setSelectedMovie(null)}
-      />
-      </>
-    );
-  }
-
-  if (movies.length === 0) {
-    return ( 
-      <>
-      <button
-      onClick={() => {
-        setUser(null);
-      }}
-      > 
-      Logout
-      </button>
-    <div>The list is empty!</div>;
-    </>
-    );
-  }
 
   return (
-    <div>
-      <button
-      onClick={() => {
-        setUser(null);
-      }}
-      > 
-      Logout
-      </button>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />
-      ))}
-    </div>
+    <Row className="justify-content-md-center">
+      {!user ? (
+        <Col md={5}>
+          <LoginView
+            onLoggedIn={(user, token) => {
+              setUser(user);
+              setToken(token);
+            }}
+          />
+          or
+          <SignupView />
+        </Col>
+
+      ) : selectedMovie ? (
+        <Col md={8} style={{ border: "1px solid black" }}>
+          <MovieView
+            style={{ border: "1px solid green" }}
+            movie={selectedMovie}
+            onBackClick={() => setSelectedMovie(null)}
+          />
+        </Col>
+      ) : movies.length === 0 ? (
+        <div>The list is empty!</div>
+      ) : 
+      (
+        <>
+          {movies.map((movie) => (
+            <Col className="mb-5" key={movie.id} md={3}>
+              <MovieCard
+                movie={movie}
+                onMovieClick={(newSelectedMovie) => {
+                  setSelectedMovie(newSelectedMovie);
+                }}
+              />
+            </Col>
+          ))}
+        </>
+      )}
+    </Row>
   );
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,3 +1,5 @@
+import './movie-view.scss';
+
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
@@ -20,7 +22,12 @@ export const MovieView = ({ movie, onBackClick }) => {
         <span>Director: </span>
         <span>{movie.director}</span>
       </div>
-      <button onClick={onBackClick}>Back</button>
+      <button onClick={onBackClick}
+        className="back-button"
+        style={{ cursor: "pointer" }} //Here, you set style to be a JavaScript object, which is why you have double curly braces 
+        > 
+          Back
+      </button>
     </div>
   );
 };
